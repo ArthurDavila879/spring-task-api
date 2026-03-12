5cdc5dd7838b6681cf68547087284eb6ccc2ae24
Arthur D'Avila <arthurdavila879@gmail.com>
2026-03-12 18:56:34 -0300

    Commit de inicializacao e criacao das pastas

---
+++ src/main/java/projeto/spring/ArthurApplication.java
@@ -0,0 +1,13 @@
+package projeto.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthurApplication.class, args);
+	}
+
+}
